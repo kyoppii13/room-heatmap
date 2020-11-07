603bc35f365430aebd2075b2bf3cb7d9a09b5e8c
kyoppii13 <kyou1013@gmail.com>
2020-11-07 22:14:12 +0900

    update crawling by using dynamodb

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -4,6 +4,8 @@ import logging
 import requests
 import lxml.html
 from downloader_tasks import download
+import boto3
+import uuid
 
 
 def main():
@@ -14,15 +16,24 @@ def main():
     start_url = 'https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&pc=30&smk=&po1=25&po2=99&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sc=13113&ta=13&cb=0.0&ct=10.0&et=9999999&mb=0&mt=9999999&cn=9999999&fw2='
 
     response = session.get(start_url)
-    last_page = get_last_page(response)
-    for page in range(1, last_page + 1):
+    # last_page = get_last_page(response)
+    sqs = boto3.resource('sqs')
+    queue = sqs.create_queue(QueueName='download')
+    for page in range(1, 2):
         url = f'{start_url}&page={page}'
         logging.info(f'Fetching list page: {url}')
         response = session.get(url)
         urls = scrape_list_page(response)
         logging.info(urls)
-        for url in urls:
-            download.delay(url)
+        messages = [{'Id': str(uuid.uuid4()), 'MessageBody': url}
+                    for url in urls]
+        # division 10
+        division_per_count = 10
+        divided_messages = [messages[i:i + division_per_count]
+                            for i in range(0, len(messages), division_per_count)]
+        for data in divided_messages:
+            response = queue.send_messages(Entries=data)
+            logging.info(response)
 
 
 def scrape_list_page(response: requests.Response) -> Iterator[str]:
@@ -36,7 +47,8 @@ def scrape_list_page(response: requests.Response) -> Iterator[str]:
 
 def get_last_page(response: requests.Response) -> int:
     html = lxml.html.fromstring(response.text)
-    last_page_str = html.cssselect('ol.pagination-parts > li:last-child > a')[0].text_content()
+    last_page_str = html.cssselect(
+        'ol.pagination-parts > li:last-child > a')[0].text_content()
     logging.info(f'last page: {last_page_str}')
     return int(last_page_str)
 
